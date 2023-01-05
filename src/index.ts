f04aa39bde4f10fde26326725825393185aeaed4
Farul Ivan <farulivan@gmail.com>
2023-01-05 17:06:17 +0700

    create class User with data and some method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello bro!');
+import { User } from './models/User';
+
+const user = new User({ name: 'Ahmad', age: 17 });
+
+user.set({ name: 'Ihsan', age: 20 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+// event listener
+user.on('change', () => {
+  console.log('Change #1');
+});
+
+user.on('change', () => {
+  console.log('Change #2');
+});
+
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+//trigger event
+user.trigger('change');
